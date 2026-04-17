de9a63578ff2575e049798976c537efc69cb9d20
wadud115 <wadud871226@gmail.com>
2026-04-17 19:08:46 +0600

    navber Responsive issue fixed

--- src/component/sheard/Footer/Footer.jsx
+++ src/component/sheard/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
             <footer className="bg-[#244D3F] text-white py-10">
 
-      <div className="max-w-4xl mx-auto flex flex-col items-center gap-6 text-center">
+      <div className=" mx-auto flex flex-col items-center gap-6 text-center">
 
         {/* Logo + Description */}
         <div>
@@ -18,19 +18,20 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Social Links */}
+        
         <div>
           <p className="text-sm mb-3">Social Links</p>
 
           <div className="flex justify-center gap-4">
-            <a className="btn btn-circle bg-white border-none"></a>
-            <a className="btn btn-circle bg-white border-none"></a>
-            <a className="btn btn-circle bg-white border-none"></a>
+            
+            <img src="/public/Facebook (1).png" alt="" />
+            <img src="/public/Instagram (1).png " alt="" />
+            <img src="/public/Twitter (1).png " alt="" />
           </div>
         </div>
 
         {/* Bottom */}
-        <div className="text-xs text-gray-300 flex flex-col md:flex-row gap-[350px] items-center">
+        <div className="text-xs text-gray-300 flex flex-col md:flex-row md:gap-10  items-center">
 
           <p>© 2026 KeenKeeper. All rights reserved.</p>
 
